cfd907466127e0241a6864da2c0c8b41fe531a48
Pintu Giri <mrpintugiri@gmail.com>
2024-10-14 17:55:40 +0530

    solution added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import { render } from "enzyme";
 
 
+const rootElement = document.getElementById('root');
 
+// Render the App component on the root DOM node
+ReactDOM.render(<App />, rootElement);
